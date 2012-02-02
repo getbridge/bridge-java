f9bca715cb9b6610567379a112e21631f2a3727e
Sridatta Thatipamala <sthatipamala@gmail.com>
2012-02-02 13:27:58 -0800

    Replace getMethod with polymorphic method lookup.
    Turns out Java's Class.getMethod requires exact method signatures and is not aware of polymorphism. This forces us to enumerate all a class's methods and check against each parameter list. This is bound to be slow. Adding caching should be a priority.

--- src/main/com/flotype/bridge/Executor.java
+++ src/main/com/flotype/bridge/Executor.java
@@ -24,34 +24,35 @@ class Executor {
 
 	// TODO synchronize this. Being invoked from different consumer threads
 	protected void execute(final Request req){
-		List<String> pathchain = req.getPathchain();
+		Reference reference = req.getReference();
 
-		String serviceName = pathchain.get(2);
-		String methodName = pathchain.get(3);
-		
+		String serviceName = reference.getServiceName();
+		String methodName = reference.getMethodName();
+
 		Utils.info(serviceName + ":" + methodName + " called");
 
 
 		final Service service = services.get(serviceName);
-
-		try {
-			final Method m = serviceToClass.get(service).getMethod(methodName, req.getParameterList());
-			tp.execute(new Runnable(){
-				public void run() {
-					try {
-						m.invoke(service, req.getArguments());
-					} catch (IllegalArgumentException e) {
-						e.printStackTrace();
-					} catch (IllegalAccessException e) {
-						e.printStackTrace();
-					} catch (InvocationTargetException e) {
-						e.printStackTrace();
-					}
-				}
-			});
-		} catch(NoSuchMethodException e){
-			e.printStackTrace();
+		
+		if(service == null){
+			Utils.error("No such service: " + serviceName);
+			return;
 		}
+
+		final Method m = getConformingMethod(methodName, req.getArguments(), serviceToClass.get(service));
+		tp.execute(new Runnable(){
+			public void run() {
+				try {
+					m.invoke(service, req.getArguments());
+				} catch (IllegalArgumentException e) {
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					e.printStackTrace();
+				}
+			}
+		});
 
 	}
 
@@ -64,6 +65,36 @@ class Executor {
 		// TODO Auto-generated method stub
 		Service s = services.get(key);
 		addService(channelName, s);
+	}
+
+	
+	
+	protected Method getConformingMethod(String methodName, Object[] arguments, Class<?> cls) {
+		Method[] publicMethods = cls.getMethods();
+		Method m = null;
+		int idxMethod = 0;
+		while ((m == null) && (idxMethod < publicMethods.length)) {
+			m = publicMethods[idxMethod];
+			if (m.getName().equals(methodName)) {
+				Class<?>[] formalParameters = m.getParameterTypes();
+				if (arguments.length == formalParameters.length) {
+					int idxParam = 0;
+					while ((m != null) && (idxParam < formalParameters.length)) {
+						Class<?> param = formalParameters[idxParam];
+						if (!param.isAssignableFrom(arguments[idxParam].getClass())) {
+							m = null;
+						}
+						idxParam++;
+					}
+				} else {
+					m = null;
+				}
+			} else {
+				m = null;
+			}
+			idxMethod++;
+		}
+		return m;
 	}
 
 }
