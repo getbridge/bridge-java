32617d40672ca5ab4f63fe9d7821b4484cb9278d
Sridatta Thatipamala <sthatipamala@gmail.com>
2012-02-01 23:01:20 -0800

    serializer cleanup in Reference.invokeRpc

--- src/main/com/flotype/bridge/Reference.java
+++ src/main/com/flotype/bridge/Reference.java
@@ -61,67 +61,36 @@ public class Reference {
 
 	public void invokeRPC(String methodName, Object ... args) throws IOException {
 
-		// Ugly trick: pass refList to the serializer to be populated
-		ObjectMapper argsMapper = new ObjectMapper();
+		ObjectMapper mapper = new ObjectMapper();
 		SimpleModule module = new SimpleModule("NowSerializers", new Version(0, 1, 0, "alpha"));
 		module.addSerializer(new ReferenceSerializer(Reference.class))
-			.addSerializer(new ServiceSerializer(Service.class))
-			.addSerializer(new MapSerializer(Map.class))
-			.addSerializer(new IntegerSerializer(Integer.class))
-			.addSerializer(new FloatSerializer(Float.class))
-			.addSerializer(new DoubleSerializer(Double.class))
-			.addSerializer(new ListSerializer(List.class))
-			.addSerializer(new BooleanSerializer(Boolean.class))
-			.addSerializer(new StringSerializer(String.class));
-		argsMapper.registerModule(module);
+			.addSerializer(new ServiceSerializer(Service.class));
+		mapper.registerModule(module);
 
-		String argsString = argsMapper.writeValueAsString(args);
-
+		
 		// Construct the request body here
 		Map<String, Object> sendBody = new HashMap<String, Object>();
 
-		ArrayList<String> destinationPath = new ArrayList(this.getPathchain());
-		destinationPath.add(methodName);
-		Reference destination = ReferenceFactory.getFactory().generateReference(destinationPath);
-		destination.setRoutingPrefix(this.getRoutingPrefix());
-
+		Reference destination = ReferenceFactory.getFactory().generateReference(this);
+		destination.setMethodName(methodName);
+		
 		sendBody.put("destination", destination);
 		sendBody.put("method", methodName);
-		sendBody.put("args", argsString);
+		sendBody.put("args", args);
 		// TODO
-		sendBody.put("exceptions", Reference.Null);
+		sendBody.put("exceptions", null);
 
-		ObjectMapper sendMapper = new ObjectMapper();
-		SimpleModule sendModule = new SimpleModule("Send", new Version(0, 1, 0, "alpha"));
-		sendModule.addSerializer(new ReferenceSerializer(Reference.class))
-			.addSerializer(new SendSerializer(Map.class));
-		sendMapper.registerModule(sendModule);
-		String sendString = sendMapper.writeValueAsString(sendBody);
-
-
-
+	
 
 		// Construct the request body here
 		Map<String, Object> commandBody = new HashMap<String, Object>();
 
 		commandBody.put("command", "SEND");
-		commandBody.put("data", sendString);
+		commandBody.put("data", sendBody);
 
-
-		ObjectMapper commandMapper = new ObjectMapper();
-		SimpleModule commandModule = new SimpleModule("Command", new Version(0, 1, 0, "alpha"));
-		commandModule.addSerializer(new CommandSerializer(Map.class));
-		commandMapper.registerModule(commandModule);
-
-		String commandString = commandMapper.writeValueAsString(commandBody);
+		String commandString = mapper.writeValueAsString(commandBody);
 
 		client.write(commandString);
-
-		// TODO Use the AMQP BasicProperties builder
-		//properties.setHeaders(linkMap);
-
-		// basicPublish(java.lang.String exchange, java.lang.String routingKey, AMQP.BasicProperties props, byte[] body)
-		//this.channel.basicPublish(Utils.Prefix.TOPIC+id.toString(), Utils.Prefix.NAMESPACED_ROUTING + this.networkAddress, properties, bodyString.getBytes());
 	}
 
 }
