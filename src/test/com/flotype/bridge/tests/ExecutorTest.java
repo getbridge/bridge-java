9c34497d7b8062965e6a9caa2a786acb1a1ee23f
Sridatta Thatipamala <sthatipamala@gmail.com>
2012-02-11 16:40:53 -0800

    create ExecutorTest JUnit class with stub tests

---
+++ src/test/com/flotype/bridge/tests/ExecutorTest.java
@@ -0,0 +1,49 @@
+package com.flotype.bridge.tests;
+
+import junit.framework.TestCase;
+
+public class ExecutorTest extends TestCase {
+
+	public void testExecutor() {
+		fail("Not yet implemented");
+	}
+
+	public void testExecute() {
+		fail("Not yet implemented");
+		
+		// No existent service
+		
+		// Nonexistent method
+		
+		// Service + method both exist
+	}
+
+	public void testAddService() {
+		fail("Not yet implemented");
+		
+		// Add service with key that didn't exist
+		
+		// Add service with key that existed before
+	}
+
+	public void testGetService() {
+		fail("Not yet implemented");
+		
+		// Get a service that exists
+		
+		// Get a service that does not exist
+	}
+
+	public void testAddExistingServiceByKey() {
+		fail("Not yet implemented");
+		
+		// Key that exists
+		
+		// Key that does not exist
+	}
+
+	public void testGetConformingMethod() {
+		fail("Not yet implemented");
+	}
+
+}
