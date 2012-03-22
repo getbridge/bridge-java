e159899d060d10e6033f94f1576bc846fd50f0de
Sridatta Thatipamala <sthatipamala@gmail.com>
2012-03-22 14:57:46 -0700

    switch Service and ServiceClient from classes to dummy interfaces

--- src/main/com/flotype/bridge/ServiceClient.java
+++ src/main/com/flotype/bridge/ServiceClient.java
@@ -1,24 +1,3 @@
 package com.flotype.bridge;
-import java.io.IOException;
 
-public class ServiceClient {
-	Reference reference;
-
-	public ServiceClient(Reference reference){
-		this.reference = reference;
-	}
-	
-	public Reference getReference(){
-		return reference;
-	}
-
-	protected void invokeRPC(String methodName, Object... args){
-		try {
-			this.reference.invokeRPC(methodName, args);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-}
+public interface ServiceClient {}
