5845f66ff9e7e6e346a8531d37f00c1658e66eff
Sridatta Thatipamala <sthatipamala@gmail.com>
2012-03-22 15:27:33 -0700

    move example code out of ready handler

--- src/test/com/flotype/bridge/example/TestMain.java
+++ src/test/com/flotype/bridge/example/TestMain.java
@@ -15,26 +15,21 @@ public class TestMain {
 			.setPort(8090)
 			.setApiKey("abcdefgh");
 		
-		bridge.setEventHandler(new BridgeEventHandler(){
-			public void onReady(){
-				ChatServiceClient chat = bridge.getService("chatserver", ChatServiceClient.class);
-				chat.join(
-						"lobby", 
-						new Service(){
-							public void msg(String s, String t){
-								System.out.println(s + ":" + t);
-							}
-						},
-						new Service(){
-							public void callback(ChatChannelClient l, String name){
-								System.out.println("JOINED " + name);
-								l.msg("peter piper", "picked a peck of pickled peppers");
-							}
-						}
-				);
-			}
-		});
-		
+		ChatServiceClient chat = bridge.getService("chatserver", ChatServiceClient.class);
+		chat.join(
+				"lobby", 
+				new Service(){
+					public void msg(String s, String t){
+						System.out.println(s + ":" + t);
+					}
+				},
+				new Service(){
+					public void callback(ChatChannelClient l, String name){
+						System.out.println("JOINED " + name);
+						l.msg("peter piper", "picked a peck of pickled peppers");
+					}
+				}
+		);
 		
 		bridge.connect();
 	}
