ff630fb3901c6f917763ad9f388bf66f96c93eb5
Sridatta Thatipamala <sthatipamala@gmail.com>
2012-03-22 14:57:11 -0700

    Major refactor of Bridge.java, remove ReferenceFactory and Request classes as a result

--- src/main/com/flotype/bridge/Bridge.java
+++ src/main/com/flotype/bridge/Bridge.java
@@ -6,36 +6,23 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 public class Bridge {
-
-	private static Log log = LogFactory.getLog(Bridge.class);
-
-	private TcpClient connection = new Bridge.TCPConnection();;
-
-	private String clientId;
-
-	// Secret used for reconnects
-	private String secret;
-
-	// Queue for commands before connects happen
-	private Queue<String> commandQueue = new LinkedList<String>();
-
-	// Whether handshake has occurred
-	private boolean handshaken = false;
-
-	private Executor executor = new Executor();
-
+	
 	// Options
-	String host;
-	Integer port;
-	String apiKey;
 	BridgeEventHandler eventHandler = null;
-
+	private static Log log = LogFactory.getLog(Bridge.class);
+	protected Dispatcher dispatcher = new Dispatcher(this);
+	boolean ready = false;
+	private Connection connection;
+	
 	public Bridge() {
-		ReferenceFactory.createFactory(this);
+		connection = new Connection(this);
+		
 		this.setHost(Utils.DEFAULT_HOST);
 		this.setPort(Utils.DEFAULT_PORT);
 		this.setEventHandler(Utils.DEFAULT_EVENT_HANDLER);
 		this.setReconnect(Utils.DEFAULT_RECONNECT);
+		
+		dispatcher.storeObject("system", new SystemService(this, dispatcher));
 	}
 
 	public Bridge(String host, Integer port, String apiKey, BridgeEventHandler eventHandler, boolean reconnect) {
@@ -48,195 +35,83 @@ public class Bridge {
 	}
 
 	public Bridge(String host, String apiKey, BridgeEventHandler eventHandler, boolean reconnect) {
-		this(host, -1, apiKey, eventHandler, reconnect);
+		this(host, null, apiKey, eventHandler, reconnect);
 	}
 
-	public boolean connect() {
-		if(this.port != -1) {
-			// Setup TCP
-			connection.setAddress(new InetSocketAddress(host, port));
-
-			try {
-				connection.start();
-			} catch (IOException e) {
-				e.printStackTrace();
-				return false;
-			}
-		} else {
-			this.sendGetRequest(this.host);
-		}
-
-		executor.addService("system", new SystemService(this, executor));
-
+	public boolean connect() throws IOException {
+		this.connection.connect();
 		return true;
-	}
-
-	public <T extends ServiceClient > T getService(String serviceName, Class<T> serviceClass){
-		Reference result = new Reference(null, this);
-		result.setRoutingPrefix("named");
-		result.setRoutingId(serviceName);
-		result.setServiceName(serviceName);
-
-		Constructor ctor;
-		try {
-			ctor = serviceClass.getConstructor(Reference.class);
-			return (T) ctor.newInstance(result);
-		} catch (Exception e) {
-			// One of the billion reflection things has gone wrong
-			e.printStackTrace();
-			return null;
-		}
-	}
-	public <T extends ServiceClient > T getChannel(String channelName, Class<T> serviceClass){
-		Map<String, Object> getChannelBody = new HashMap<String, Object>();
-		getChannelBody.put("name", channelName);
-		sendCommand("GETCHANNEL", getChannelBody);
-
-		Reference result = getChannelReference(channelName);
-		
-		Constructor ctor;
-		try {
-			ctor = serviceClass.getConstructor(Reference.class);
-			return (T) ctor.newInstance(result);
-		} catch (Exception e) {
-			// One of the billion reflection things has gone wrong
-			e.printStackTrace();
-			return null;
-		}
+	}	
+	
+	protected void send(Reference destination, Object[] args) {
+		String msg = JSONCodec.createSEND(this, destination, args);
+		this.connection.send(msg);
 	}
 	
-	protected Reference getChannelReference(String channelName) {
-		Reference result = new Reference(null, this);
-		result.setRoutingPrefix("channel");
-		result.setRoutingId(channelName);
-		result.setServiceName("channel:" + channelName);
-		return result;
-	}
-
 	public void publishService(String name, Service service) {
 		publishService(name, service, null);
 	}
 
-	public void publishService(String name, Service service, Service callback) {
+	public void publishService(String name, Service service, Callback callback) {
 
 		if(name.equals("system")) {
 			log.error("Invalid service name: " + name);
 			return;
 		}
 
-		executor.addService(name, service);
-		service.createReference(name);
-		joinWorkerPool(name, callback);
+		Reference serviceRef = dispatcher.storeObject(name, service);
+		Reference callbackRef = dispatcher.storeRandomObject(callback);
+		String msg = JSONCodec.createJWP(this, name, serviceRef, callbackRef);
+		this.connection.send(msg);
+	}
+	
+	public <T> T getService(String serviceName, Class<T> serviceClass){
+		Reference result = Reference.createServiceReference(this, serviceName, Utils.getMethods(serviceClass));
+		return (T) Utils.createProxy(result, serviceClass);
 	}
 
-	protected void publishService(Service service){
-		String name = Utils.generateId();
-		service.createReference(name);
-		executor.addService(name, service);
+	public <T> T getChannel(String channelName, Class<T> serviceClass){
+		String msg = JSONCodec.createGETCHANNEL(this, channelName);
+		this.connection.send(msg);
+		
+		Reference result = Reference.createChannelReference(this, channelName, Utils.getMethods(serviceClass));
+		return (T) Utils.createProxy(result, serviceClass);
 	}
-
-	public void joinWorkerPool(String name, Service callback) {
-
-		Map<String, Object> joinWorkerPoolBody = new HashMap<String, Object>();
-
-		joinWorkerPoolBody.put("name", name);
-
-		if(callback != null) {
-			joinWorkerPoolBody.put("callback", callback);
-		}
-
-		this.sendCommand("JOINWORKERPOOL", joinWorkerPoolBody);
-	}
-
+	
 	public void joinChannel(String name, Service handler) {
 		joinChannel(name, handler, null);
 	}
-
-	public void joinChannel(String name, Service handler, Service callback) {
-		Map<String, Object> joinChannelBody = new HashMap<String, Object>();
-
-		joinChannelBody.put("name", name);
-		joinChannelBody.put("handler", handler);
-
-		if(callback != null) {
-			joinChannelBody.put("callback", callback);
-		}
-
-		this.sendCommand("JOINCHANNEL", joinChannelBody);
-	}
 	
+	public void joinChannel(String name, Service handler, Service callback) {
+		Reference handlerRef = dispatcher.storeObject("channel:"+name, handler);
+		Reference callbackRef = dispatcher.storeRandomObject(handler);
+		String msg = JSONCodec.createJC(this, name, handlerRef, callbackRef);
+		this.connection.send(msg);
+	}
+
 	public void leaveChannel(String name, Service handler) {
 		leaveChannel(name, handler, null);
 	}
 	
 	public void leaveChannel(String name, Service handler, Service callback) {
-		Map<String, Object> leaveChannelBody = new HashMap<String, Object>();
-
-		leaveChannelBody.put("name", name);
-		leaveChannelBody.put("handler", handler);
-		
-		if(callback != null) {
-			leaveChannelBody.put("callback", callback);
-		}
-
-		this.sendCommand("LEAVECHANNEL", leaveChannelBody);
-	}
-
-
-	private void sendCommand(String command, Map<String, Object> data){
-		ObjectMapper mapper = new ObjectMapper();
-		SimpleModule module = new SimpleModule("Handler", new Version(0, 1, 0, "alpha"));
-		module.addSerializer(new ReferenceSerializer(Reference.class))
-		.addSerializer(new ServiceSerializer(Service.class))
-		.addSerializer(new ServiceClientSerializer(ServiceClient.class));
-		mapper.registerModule(module);
-
-		try {
-
-			// Construct the request body here
-			Map<String, Object> commandBody = new HashMap<String, Object>();
-
-			commandBody.put("command", command);
-			commandBody.put("data", data);
-
-			String commandString = mapper.writeValueAsString(commandBody);
-
-			if(!this.handshaken && !command.equals("CONNECT")) {
-				this.addCommandQueue(commandString);	
-			} else {
-				this.write(commandString);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-
-	protected Executor getExecutor() {
-		return this.executor;
+		Reference handlerRef = dispatcher.storeObject(name, handler);
+		Reference callbackRef = dispatcher.storeRandomObject(callback);
+		String msg = JSONCodec.createLEAVECHANNEL(this, name, handlerRef, callbackRef);
+		this.connection.send(msg);
 	}
 
 	public Bridge setApiKey(String apiKey) {
-		this.apiKey = apiKey;
+		this.connection.setApiKey(apiKey);
 		return this;
 	}
 
-	private void setSecret(String secret) {
-		this.secret = secret;
-	}
-
-	private String getSecret() {
-		return this.secret;
-	}
-
-
 	public Bridge setHost(String host) {
-		this.host = host;
+		this.connection.setHost(host);
 		return this;
 	}
 
 	public Bridge setPort(Integer port) {
-		this.port = port;
+		this.connection.setPort(port);
 		return this;
 	}
 
@@ -249,20 +124,27 @@ public class Bridge {
 		this.connection.setReconnect(reconnect);
 		return this;
 	}
-
-	protected void setClientId(String clientId) {
-		this.clientId = clientId;
+	
+	protected void onDisconnect(){
+		this.ready = false;
+	}
+	
+	protected void onReady(){
+		this.ready = true;
+		dispatcher.fixClientIds(this.connection.clientId);
+		this.eventHandler.onReady();
 	}
 
-	public String getClientId () {
-		return this.clientId;
+	protected void onReconnect() {
+		
 	}
 
-	public boolean isHandshaken() {
-		return this.handshaken;
+	public void onConnected() {
+		
 	}
 
+	protected String getClientId() {
+		return connection.clientId;
 	}
-
-
+	
 }
