7438a191e61a9c08bf6a846941e34ef3c82d5b24
Sridatta Thatipamala <sthatipamala@gmail.com>
2012-06-07 13:41:14 -0700

    change Connection to closely mirror the "reference" implementation in JS/Python/Ruby

--- bridge-java-core/src/main/java/com/flotype/bridge/Connection.java
+++ bridge-java-core/src/main/java/com/flotype/bridge/Connection.java
@@ -1,23 +1,25 @@
 package com.flotype.bridge;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.InetSocketAddress;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.ByteBuffer;
-import java.util.LinkedList;
+import java.net.MalformedURLException;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-import net.bobah.nio.TcpClient;
-
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Connection extends TcpClient {
+import com.flotype.bridge.network.HTTPConnection;
+import com.flotype.bridge.network.Socket;
+import com.flotype.bridge.network.SocketBuffer;
+import com.flotype.bridge.network.TCPSocket;
+import com.flotype.bridge.network.SSLSocket;
+
+public class Connection {
 	private static Logger log = LoggerFactory.getLogger(Connection.class);
 
 	String clientId;
@@ -25,180 +27,156 @@ public class Connection extends TcpClient {
 	// Secret used for reconnects
 	private String secret;
 
-	// Queue for commands before connects happen
-	private Queue<String> commandQueue = new LinkedList<String>();
-
 	private boolean handshaken;
 
+	// Options
 	private Bridge bridge;
 	private String apiKey = null;
 	private String host = null;
 	private int port = -1;
-	private String redirector;
+	private String redirector = Utils.DEFAULT_REDIRECTOR;
+
+	private Socket sock;
+	private TCPSocket tcpSock;
+	private SocketBuffer sockBuffer;
+
+	private long reconnectInterval;
+	private static final ScheduledExecutorService reconnectExecutor = 
+		  Executors.newSingleThreadScheduledExecutor();
+
+	private boolean reconnect;
+
+	private boolean secure;
 
 	protected Connection(Bridge bridge) {
 		this.bridge = bridge;
+		reconnectInterval = 400;
+		
+		sockBuffer = new SocketBuffer();
+		tcpSock = new TCPSocket(this);
+		sock = sockBuffer;
 	}
 
-	protected void connect() throws IOException {
+	protected void start() throws IOException {
 		if (this.host == null || this.port == -1) {
 			redirector();
 		} else {
-			this.setAddress(new InetSocketAddress(host, port));
-			log.info("Starting TCP connection {} {}", this.host, this.port);
-			start();
+			establishConnection();
+		}
+	}
+
+	private void establishConnection() {
+		log.info("Starting TCP connection {} {}", this.host, this.port);
+		tcpSock.connect(host, port);
+	}
+
+	private void redirector() throws MalformedURLException {
+		String endpoint = redirector + "/redirect/" + apiKey;
+		HTTPConnection redirectorRequest = new HTTPConnection(this, endpoint);
+		redirectorRequest.connect();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void onRedirectorResponse(String json) throws JsonParseException, JsonMappingException, IOException {
+		Map<String, Object> response = JSONCodec.parseRedirector(json);
+		Map<String, Object> data = (Map<String, Object>) response.get("data");
+		if(data == null) {
+			log.error("Unable to parse redirector response");
+			return;
+		}
+		
+		if(data.get("bridge_port") == null || data.get("bridge_host") == null) {
+			log.error("Could not find host and port in JSON body");
+		} else {
+			host = (String) data.get("bridge_host");
+			port =  Integer.parseInt((String) data.get("bridge_port"));
+			establishConnection();
+		}
+		
+	}
+	
+	private void reconnect() {
+		log.info("Attempting to reconnect");
+		if(reconnectInterval < 32768) {
+			Runnable task = new Runnable(){
+				public void run(){
+					reconnectInterval *= 2;
+					establishConnection();
+				}
+			};
+			
+			reconnectExecutor.schedule(task, reconnectInterval, TimeUnit.MILLISECONDS);
 		}
 	}
 	
-	protected void send(String string) {
-		if (bridge.ready) {
-			write(string.getBytes());
-		} else {
-			// Buffer messages until reconnection happens
-			commandQueue.add(string);
-		}
+	public void send(String msg){
+		sock.send(msg);
 	}
 
-	public void write(byte[] buffer) {
-		log.info("Sending {}", new String(buffer));
-		ByteBuffer data = ByteBuffer.allocate(buffer.length + 4);
-		data.put(Utils.intToByteArray(buffer.length)); // Put the length header
-		data.put(buffer); // Put the data
-		data.flip();
-		try {
-			this.send(data);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void redirector() {
-		String result = null;
-		if (redirector.startsWith("http://")) {
-			try {
-				// Send data
-				String urlStr = redirector;
-				if (urlStr.charAt(urlStr.length() - 1) != '/') {
-					urlStr += '/';
-				}
-				urlStr = urlStr + "redirect/" + this.apiKey;
-				URL url = new URL(urlStr);
-				URLConnection conn = url.openConnection();
-
-				// Get the response
-				BufferedReader rd = new BufferedReader(new InputStreamReader(
-						conn.getInputStream()));
-				StringBuffer sb = new StringBuffer();
-				String line;
-				while ((line = rd.readLine()) != null) {
-					sb.append(line);
-				}
-				rd.close();
-				result = sb.toString();
-
-				// Parse response JSON, set fields on self, and connect
-				Map<String, Object> jsonObj = JSONCodec.parseRedirector(result);
-				Map<String, String> data = (Map<String, String>) jsonObj
-						.get("data");
-				
-				if(data.get("bridge_host") == null || data.get("bridge_port") == null) {
-					log.error("Could not find host and port in JSON body");
-					return;
-				}
-				
-				this.host = data.get("bridge_host");
-				this.port = Integer.parseInt(data.get("bridge_port"));
-				this.connect();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	private void processCommandQueue() {
-		for (String str : commandQueue) {
-			// Replace the 'null' in the JSON reference address with the
-			// now-known client ID
-			this.send(str.replace("\"ref\":[\"client\",null,",
-					"\"ref\":[\"client\",\"" + this.clientId + "\","));
-		}
-		commandQueue.clear();
-	}
-
-	@Override
-	protected void onRead(ByteBuffer buf) throws Exception {
-		while (buf.hasRemaining()) {
-			if(buf.remaining() < 4) {
-				// Full header not received yet. Wait until next time
-				break;
-			}
-			int length = buf.getInt();
-			if (buf.remaining() < length) {
-				// Header received but not the body. Wait until next time.
-				// Reset the ByteBuffer position to the start of the header.
-				int pos = buf.position() - 4;
-				buf.position(pos);
-				break;
-			}
-			byte[] body = new byte[length];
-			buf.get(body);
-			String bodyString = new String(body);
-			if (length != body.length) {
-				throw new Exception(
-						"Expected message length not equal to buffer size");
-			}
-			
-			log.info("Received {}", bodyString);
-
-			if (!bridge.ready) {
-				// Client not handshaken
-				String[] ids = (bodyString).split("\\|");
-				if (ids.length == 2) {
-					// Got a ID and secret as response
-					log.info("clientId receieved {}", ids[0]);
-					clientId = ids[0];
-					secret = ids[1];
-
-					if(this.handshaken == false) {
-						bridge.onReady();
-						this.handshaken = true;
-					} else {
-						bridge.onReconnect();
-					}
-					
-					processCommandQueue();
-					log.info("Handshake complete");
-					return;
-				}
-			}
-
-			// Parse as normal
-			Map<String, Object> message = Utils.deserialize(bridge, body);
-			if(message.get("destination") == null) {
-				log.warn("No destination in message {}", bodyString);
-			} else {
-			bridge.dispatcher.execute((Reference) message.get("destination"),
-					(List<Object>) message.get("args"));
-			}
-		}
-	}
-
-	@Override
-	protected void onDisconnected() {
-		log.warn("Connection closed");
-		bridge.onDisconnect();
-	}
-
-	@Override
-	protected void onConnected() throws Exception {
+	public void onOpen() {
 		log.info("Beginning handshake");
 		String connectString = JSONCodec.createCONNECT(bridge, clientId,
 				secret, apiKey);
-		// Use write instead of send for unbuffered sending
-		this.write(connectString.getBytes());
+		tcpSock.send(connectString);
 		bridge.onConnected();
+	}
+	public void onClose() {
+		log.warn("Connection closed");
+		bridge.onDisconnect();
+		sock = sockBuffer;
+		if(reconnect == true) {
+			reconnect();
+		}
+	}
+
+	public void onMessage(String message) {
+		log.info("Received {}", message);
+
+		String[] ids = message.split("\\|");
+		if (ids.length == 2) {
+			// Got a ID and secret as response
+			log.info("clientId receieved {}", ids[0]);
+			clientId = ids[0];
+			secret = ids[1];
+			reconnectInterval = 400;
+			sockBuffer.processQueue(tcpSock, clientId);
+			sock = tcpSock;
+
+			log.info("Handshake complete");
+
+			if(this.handshaken == false) {
+				bridge.onReady();
+				this.handshaken = true;
+			} else {
+				bridge.onReconnect();
+			}
+		} else {
+			processMessage(message);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private void processMessage(String message) {
+		Map<String, Object> obj;
+		try {
+			obj = Utils.deserialize(bridge, message.getBytes());
+			if(obj.get("destination") == null) {
+				log.warn("No destination in message {}", message);
+			} else {
+				bridge.dispatcher.execute((Reference) obj.get("destination"),
+						(List<Object>) obj.get("args"));
+			}
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	public void setApiKey(String apiKey) {
@@ -215,6 +193,15 @@ public class Connection extends TcpClient {
 
 	public void setRedirector(String redirectorUrl) {
 		this.redirector = redirectorUrl;
+	}
+
+	public void setSecure(boolean secure) {
+		this.secure = secure;
+		if(this.secure == true) {
+			tcpSock = new SSLSocket(this);
+		} else {
+			tcpSock = new TCPSocket(this);
+		}
 	}
 
 }
